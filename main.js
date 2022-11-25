03b30b932130df609202fc370a5617bea3899201
Luccas Klefens <luccas_klefens@hotmail.com>
2022-11-25 12:46:35 -0300

    Commit Inicial

---
+++ main.js
@@ -0,0 +1,20 @@
+class Voluntario {
+
+    constructor() {
+        this.id = 0
+        this.nomeVoluntario = '';
+        this.celularVoluntario = '';
+        this.emailVoluntario = '';
+    }
+
+    adicionar() {
+        alert('Vamos adicionar um voluntario')
+    }
+
+    excluir() {
+        alert('item deletado!')
+    }
+
+}
+
+var voluntario = new Voluntario();
